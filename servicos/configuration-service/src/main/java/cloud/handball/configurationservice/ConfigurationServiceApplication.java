704a306b46fd93ee0cedc288399f559d404c01cf
Emerson Henrique Soares Silva <emerson.hss@gmail.com>
2020-08-12 00:05:35 -0300

    Configuration service

---
+++ servicos/configuration-service/src/main/java/cloud/handball/configurationservice/ConfigurationServiceApplication.java
@@ -0,0 +1,17 @@
+package cloud.handball.configurationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigurationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationServiceApplication.class, args);
+	}
+
+}
